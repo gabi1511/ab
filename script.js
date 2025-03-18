510182b27bc5e38eafe3b459ffd27a76936224ad
gabi1511 <142450991+gabi1511@users.noreply.github.com>
2025-03-18 20:37:18 -0300

    s

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
 function toggleMenu() {
-    const menuHamburguer = document.getElementById("menu-hamburguer");
-    
-    if (menuHamburguer.style.display === "block") {
-        menuHamburguer.style.display = "none";
-    } else {
-        menuHamburguer.style.display = "block";
-    }
+    const sidebar = document.getElementById('sidebar');
+    sidebar.classList.toggle('hidden');
 }
